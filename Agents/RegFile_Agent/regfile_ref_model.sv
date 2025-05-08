56480a5e1db41dcd886791a826b6fe1c9aaf7934
Mohamed Ahmed Hamed <104694689+mohamedyakoub@users.noreply.github.com>
2025-05-08 15:42:03 +0300

    Update regfile_ref_model.sv

--- Agents/RegFile_Agent/regfile_ref_model.sv
+++ Agents/RegFile_Agent/regfile_ref_model.sv
@@ -1,8 +1,8 @@
 class regfile_ref_model extends uvm_subscriber #(reg_file_sequence_item);
     `uvm_component_utils(regfile_ref_model)
 
-    uvm_analysis_port #(regfile_ref_model) rf_m_port;
-    reg_file_sequence_item item;
+    uvm_analysis_port #(reg_file_sequence_item) rf_m_port;
+    reg_file_sequence_item item,tr;
     bit [31:0] regfile [0:31]; // 32 registers of 32 bits each
     function new(string name = "regfile_ref_model", uvm_component parent);
         super.new(name, parent);
@@ -14,17 +14,22 @@ class regfile_ref_model extends uvm_subscriber #(reg_file_sequence_item);
         rf_m_port = new("rf_m_port", this);
     endfunction: build_phase
     
-    virtual function  write(reg_file_sequence_item tr);
+     function void write(reg_file_sequence_item t);
         `uvm_info("RegF_M", "Packet recieved", UVM_HIGH)
             item = reg_file_sequence_item::type_id::create("item");
-			item.rdata_a_o=regfile[tr.raddr_a_i];
-			item.rdata_b_o=regfile[tr.raddr_b_i];
-			if (tr.we_a_i) begin
-				reg_file[tr.waddr_a_i] = tr.wdata_a_i;
+			
+			item.rdata_a_o=regfile[t.raddr_a_i];
+			item.rdata_b_o=regfile[t.raddr_b_i];
+			if (t.we_a_i) begin
+				regfile[t.waddr_a_i] = t.wdata_a_i;
+				`uvm_info("REGF_Model", $sformatf("Addr: %0h, Data: %0h", t.waddr_a_i, t.wdata_a_i), UVM_HIGH)
 			end 
-			if (tr.we_b_i) begin
-				reg_file[tr.waddr_b_i] = tr.wdata_b_i;
+			if (t.we_b_i) begin
+				regfile[t.waddr_b_i] = t.wdata_b_i;
+				`uvm_info("REGF_Model", $sformatf("Addr: %0h, Data: %0h", t.waddr_b_i, t.wdata_b_i), UVM_HIGH)
 			end 
+	
+	
         rf_m_port.write(item);
     endfunction
 
