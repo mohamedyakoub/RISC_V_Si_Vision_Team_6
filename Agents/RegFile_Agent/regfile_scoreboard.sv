11acbac4ff29584b73ffb7138a24d3a790540d76
Mohamed Ahmed Hamed <104694689+mohamedyakoub@users.noreply.github.com>
2025-05-08 14:25:01 +0300

    Update regfile_scoreboard.sv

--- Agents/RegFile_Agent/regfile_scoreboard.sv
+++ Agents/RegFile_Agent/regfile_scoreboard.sv
@@ -4,8 +4,6 @@ class regfile_scoreboard extends uvm_scoreboard;
     `uvm_component_utils(regfile_scoreboard)
 
     int no_trans,correct,incorrect;
-    bit corr_tran;
-    bit [5:0] C_delay_0,C_golden;
     uvm_analysis_imp_exp#(reg_file_sequence_item,regfile_scoreboard) exp_ap;
     uvm_analysis_imp_act#(reg_file_sequence_item,regfile_scoreboard) act_ap;
     reg_file_sequence_item actual[$:1],expected[$:1];
@@ -29,6 +27,7 @@ class regfile_scoreboard extends uvm_scoreboard;
     virtual function  write_act(reg_file_sequence_item item);
         `uvm_info("Scoreboard", "Packet received", UVM_HIGH)
         actual.push_back(item);
+        no_trans=no_trans+1;
         //item.print();
     endfunction
 
@@ -39,16 +38,18 @@ class regfile_scoreboard extends uvm_scoreboard;
             wait(actual.size()>0 && expected.size()>0);
             if(actual[0].rdata_a_o == expected[0].rdata_a_o && actual[0].rdata_b_o == expected[0].rdata_b_o) begin
                 `uvm_info("REGF_Scoreboard", "Transaction is correct", UVM_MEDIUM)
+                correct=correct+1;
             end
             else begin
                 `uvm_info("REGF_Scoreboard", "Transaction is incorrect", UVM_MEDIUM)
                 `uvm_report_info("REGF_Scoreboard", $sformatf("Expected: %0h, Actual: %0h", expected[0].rdata_a_o, actual[0].rdata_a_o), UVM_HIGH)
+                incorrect=incorrect+1;
             end
             actual.pop_front();
             expected.pop_front();
         end
     endtask 
-
+    
 endclass
 
 
